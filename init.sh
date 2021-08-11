01bbcf47ca7c27f3cfd6e40c7f000ce6304f55bc
Mauro Chojrin <mauro.chojrin@leewayweb.com>
2021-08-11 16:27:12 -0300

    Configuration for xdebug

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 
 set -x
 
+sudo chown www-data:www-data private.key
 docker-compose up
 docker-compose exec client composer install
 docker-compose exec server composer install
-chown -r $(docker-compose exec server id -u) private.key
